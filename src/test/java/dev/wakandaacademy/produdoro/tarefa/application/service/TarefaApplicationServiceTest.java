4f87c23e9aee5d4956ee756a855b9006073581d0
Leo <nascimentoleo899@gmail.com>
2023-09-26 21:41:00 -0300

    implementa testes na camada de service

--- src/test/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaApplicationServiceTest.java
+++ src/test/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaApplicationServiceTest.java
@@ -10,16 +10,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 
+import dev.wakandaacademy.produdoro.DataHelper;
+import dev.wakandaacademy.produdoro.handler.APIException;
+import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaDetalhadoResponse;
 import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaIdResponse;
 import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaRequest;
 import dev.wakandaacademy.produdoro.tarefa.application.repository.TarefaRepository;
 import dev.wakandaacademy.produdoro.tarefa.domain.Tarefa;
+import dev.wakandaacademy.produdoro.usuario.application.repository.UsuarioRepository;
+import dev.wakandaacademy.produdoro.usuario.domain.Usuario;
 
 @ExtendWith(MockitoExtension.class)
 class TarefaApplicationServiceTest {
@@ -31,6 +38,9 @@ class TarefaApplicationServiceTest {
     //	@MockBean
     @Mock
     TarefaRepository tarefaRepository;
+    
+    @Mock
+    UsuarioRepository usuarioRepository;
 
     @Test
     void deveRetornarIdTarefaNovaCriada() {
@@ -50,4 +60,31 @@ class TarefaApplicationServiceTest {
         TarefaRequest request = new TarefaRequest("tarefa 1", UUID.randomUUID(), null, null, 0);
         return request;
     }
+    
+    @Test
+    void deveRetornarListaTarefasPorUsuario() {
+    	Usuario usuario = DataHelper.createUsuario();
+    	List<Tarefa> tarefas = DataHelper.createListTarefa();
+        when(usuarioRepository.buscaUsuarioPorEmail(any())).thenReturn(DataHelper.createUsuario());
+        when(tarefaRepository.buscaTarefasPorUsuario(any(UUID.class))).thenReturn(tarefas);
+        List<TarefaDetalhadoResponse> response = tarefaApplicationService.buscarTarefasPorUsuario(usuario.getEmail(), usuario.getIdUsuario());
+        assertEquals(8, response.size());
+        assertNotNull(response);
+    }
+    
+    @Test
+    void deveRetornarExceptionAoListarTarefasPorUsuario() {
+        when(usuarioRepository.buscaUsuarioPorEmail(any())).thenReturn(DataHelper.createUsuario());
+        
+        APIException ex = Assertions.assertThrows(APIException.class, () -> tarefaApplicationService.buscarTarefasPorUsuario("invalido@email.com", UUID.randomUUID()));
+        assertEquals("Usuário não encontrado", ex.getMessage());
+        assertEquals(HttpStatus.BAD_REQUEST, ex.getStatusException());
+    }
+    
+    
+    
+    
+    
+    
+    
 }
