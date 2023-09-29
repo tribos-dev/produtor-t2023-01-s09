5e0786b868c9e123a9af3c6b782732cc57a6953f
Alessandra-Sousa <alessandrasousadacruz@gmail.com>
2023-09-28 22:00:23 -0300

    implementa camada de teste unitário

--- src/test/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaApplicationServiceTest.java
+++ src/test/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaApplicationServiceTest.java
@@ -1,29 +1,35 @@
 package dev.wakandaacademy.produdoro.tarefa.application.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import dev.wakandaacademy.produdoro.DataHelper;
+import dev.wakandaacademy.produdoro.tarefa.application.api.EditaTarefaRequest;
 import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaIdResponse;
 import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaRequest;
 import dev.wakandaacademy.produdoro.tarefa.application.repository.TarefaRepository;
 import dev.wakandaacademy.produdoro.tarefa.domain.Tarefa;
+import dev.wakandaacademy.produdoro.usuario.application.repository.UsuarioRepository;
+import dev.wakandaacademy.produdoro.usuario.domain.Usuario;
 
 @ExtendWith(MockitoExtension.class)
 class TarefaApplicationServiceTest {
-
+	
     //	@Autowired
     @InjectMocks
     TarefaApplicationService tarefaApplicationService;
@@ -31,6 +37,29 @@ class TarefaApplicationServiceTest {
     //	@MockBean
     @Mock
     TarefaRepository tarefaRepository;
+    
+    @InjectMocks
+    DataHelper dataHelper;
+    
+    @Mock
+    private UsuarioRepository usuarioRepository;
+    Tarefa tarefa;
+    Usuario usuario;
+    UUID idTarefa;
+    UUID idUsuario;
+    String email;
+    
+    @BeforeEach
+    void setup() {
+        tarefa = dataHelper.createTarefa();
+        usuario = dataHelper.createUsuario();
+        idTarefa = tarefa.getIdTarefa();
+        idUsuario = usuario.getIdUsuario();
+        email = usuario.getEmail();
+        when(usuarioRepository.buscaUsuarioPorEmail(anyString())).thenReturn(usuario);
+        when(tarefaRepository.buscaTarefaPorId(any(UUID.class))).thenReturn(Optional.of(tarefa));
+    }
+
 
     @Test
     void deveRetornarIdTarefaNovaCriada() {
@@ -49,5 +78,12 @@ class TarefaApplicationServiceTest {
     public TarefaRequest getTarefaRequest() {
         TarefaRequest request = new TarefaRequest("tarefa 1", UUID.randomUUID(), null, null, 0);
         return request;
+    }
+    
+    @Test
+    void testEditaTarefa() {
+        EditaTarefaRequest request = dataHelper.createEditaTarefaRequest();
+        tarefaApplicationService.editaTarefa(email, idTarefa, request);
+        verify(tarefaRepository, times(1)).salva(tarefa);
     }
 }
